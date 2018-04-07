742351c3df54be3d79d2a93c75b69891406e0b92
Tpk <admin@tcgforum.pl>
2018-04-07 23:40:43 +0200

    UP script to ver.2

--- cvhost.sh
+++ cvhost.sh
@@ -2,7 +2,7 @@
 
 #===================
 #  AUTOR: r0v
-#  DESCRIPTION: Simple nginx v-host add script
+#  DESCRIPTION: Simple nginx v-host add/remove script
 #===================
 
 # Variables
@@ -10,6 +10,8 @@ directoryRoot=/var/www/html/
 siteAvaliable=/etc/nginx/sites-available/
 siteEnabled=/etc/nginx/sites-enabled/
 logPath=/var/log/vhost/
+domainExt="local"
+domainName="example"
 
 function flash(){
 
@@ -44,13 +46,7 @@ function flash(){
     fi
 }
 
-if [ $# -eq 0 ]; then
-
-    echo "Usage: <vhost-name>"
-
-elif [ $# -eq 1 ]; then
-
-    # Starting script
+function boot(){
     clear
 
     currentData=$(date +"%F")
@@ -61,87 +57,169 @@ elif [ $# -eq 1 ]; then
         sleep 0.2
         echo -n "."
     done
-    echo -n "log was writing in ${logPath}log_${currentData}.log"
+    echo -n "log was writing in ${logPath}log_${currentData}.log" 
     echo ""
+}
 
-    # Create a root directory
-    cd $directoryRoot;
-    directoryName=$(echo $1 | tr [:upper:] [:lower:])
-    if [ -d "$directoryName.local" ]; then
-        flash -w "Warning, a file with that name exists, in $directoryRoot"
-    else
-        mkdir "$directoryName.local"
-        flash "Create a root directory."
-    fi
+if [ $# -le 1 ]; then
+    
+    # Less then or equal 1 params
+    flash -i "Usage: <action> <vhost-name>"
 
-    # Create a configuration file
-    cd /etc/nginx/sites-available/
+elif [ $# -eq 2 ]; then
+    case "$1" in 
+        "add")
+            # Init message
+            boot
 
-    confName=$directoryName
+            # Start script
 
-    if [ -e $siteAvaliable$directoryName ]; then
-        flash -d "Error! Configuration file is exists."
-    else
-        echo "server {
-                listen 80;
-                listen [::]:80;
+            if [[ $2 =~ \. ]]; then
+                domainName=$(echo $2 | sed 's/\.[a-z.]*//')
+                domainExt=$(echo $2 | sed 's/[a-z.]*\.//')
+            else
+                domainName=$(echo $2 | tr -d ./ )
+            fi
 
-                # Error, access ecetra
-                error_log /var/log/nginx/$confName.error error;
-                access_log /var/log/nginx/$confName.access;
+            # Create a root directory
+            cd $directoryRoot;
+            directoryName=$(echo "$domainName.$domainExt" | tr [:upper:] [:lower:])
+            if [ -d "$directoryName" ]; then
+                flash -w "Warning, a file with that name exists, in $directoryRoot"
+            else
+                mkdir "$directoryName"
+                flash "Create a root directory."
+            fi
 
-                root /var/www/html/$confName.local;
+            confName=$directoryName
 
-                # Add PHP support
-                index index.php index.html index.htm index.nginx.debian.html;
+            # Create a configuration file
+            cd $siteAvaliable
 
-                server_name $confName.local www.$confName.local;
+            if [ -e $siteAvaliable$confName ]; then
+                flash -d "Error! Configuration file is exists."
+            else
+                echo "server {
+        listen 80;
+        listen [::]:80;
 
-                location / {
-                    try_files \$uri \$uri/ =404;
-                }
+        # Error, access ecetra
+        error_log /var/log/nginx/$confName.error error;
+        access_log /var/log/nginx/$confName.access;
 
-                # PHP Config
-                location ~ \.php$ {
-                    include snippets/fastcgi-php.conf;
-                    fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
-                }
-            }" > $siteAvaliable$directoryName
-        flash "Create a configuration file"
-    fi
+        root /var/www/html/$confName;
 
-    # Edit hosts file
-    sudo echo "127.0.0.1    localhost.localdomain $confName.local www.$confName.local" >> /etc/hosts # TODO: change on sed
-    flash "Edit hosts file"
+        # Add PHP support
+        index index.php index.html index.htm index.nginx.debian.html;
 
-    # Add symlink
-    if [ -h $siteEnabled$confName ]; then
-        flash -w "Sorry, I can't add a symlink which exists!"
-    else
-        sudo ln -s $siteAvaliable$confName $siteEnabled$confName
-        flash "Add symlink correct"
-    fi
+        server_name $confName www.$confName;
 
-    # Create sample file & other stuff
-    if [ ! -d $logPath ]; then
-        sudo mkdir $logPath
-    fi
+        location / {
+            try_files \$uri \$uri/ =404;
+        }
 
-    if [ -d "$directoryRoot$directoryName.local" ]; then
-        echo "<?php
-                phpinfo();
-            ?>" > $directoryRoot$directoryName.local/index.php
-    fi
-    flash "Create a sample PHP file"
+        # PHP Config
+        location ~ \.php$ {
+            include snippets/fastcgi-php.conf;
+            fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
+        }
+}" > $siteAvaliable$confName
+                flash "Create a configuration file"
+            fi
 
-    # Reload ngnix
-    sudo service nginx restart |& tee -a "${logPath}log_${currentData}.log" &> /dev/null
-    flash "Reload nginx server"
+            # Edit hosts file
+            sudo echo "127.0.0.1    localhost.localdomain $confName www.$confName" >> /etc/hosts # TODO: change on sed
+            flash "Edit hosts file"
 
-    flash -o "Nice work! Your v-host is now active, you can looking this at the http://$confName.local/"
+            # Add symlink
+            if [ -h $siteEnabled$directoryName ]; then
+                flash -w "Sorry, I can't add a symlink which exists!"
+            else
+                sudo ln -s $siteAvaliable$directoryName $siteEnabled$directoryName
+                flash "Add symlink correct"
+            fi
 
-else
+            # Create sample file & other stuff
+            if [ ! -d $logPath ]; then
+                sudo mkdir $logPath
+            fi
 
-    flash -d "Something went wrong! Please try again, remember about params..."
+            if [ ! -f "$directoryRoot$directoryName/index.php" ]; then
+                echo "<?php
+                        phpinfo();
+                    ?>" > "$directoryRoot$directoryName/index.php"
+                flash "Create a sample PHP file"
+            else
+                flash -d "Example page file exits!"
+            fi
 
+            # Add from list pages
+            ### NEW FEATURE v3.0 ###
+
+            # Reload ngnix
+            sudo service nginx restart |& tee -a "${logPath}log_${currentData}.log" &> /dev/null
+            flash "Reload nginx server"
+
+            flash -o "Nice work! Your v-host is now active, you can looking this at the http://$confName/"
+
+        ;;
+        "rem")
+
+            remName="$2.mira"
+
+            cd $directoryRoot
+            if [ -d "$remName" ]; then
+                echo -n "Are you sure? Delete $remName? (Yes/No) "
+                read tester
+                if [[ "$tester" =~ [yY] || [yY][eE][sS] ]]; then
+
+                    # Remove a root directory
+                    sudo rm -r $remName
+                    flash "Remove a root directory"
+
+                    # Remove a configuration file
+                    cd $siteAvaliable
+                    if [ -f "$remName" ]; then 
+                        sudo rm $remName
+                        flash "Remove a configuration file"
+                    else
+                        flash -d "Error! Config file was not removed."
+                    fi
+
+                    # Edit hosts file
+                    sudo sed -i "/127.0.0.1    localhost.localdomain $remName www.$remName/d" /etc/hosts # TODO // security 
+                    flash "Edit hosts file"
+
+                    # Remove symlink
+                    cd $siteEnabled
+                    if [ -h "$remName" ]; then 
+                        sudo rm $remName
+                        flash "Remove symlink"
+                    else
+                        flash -d "Error! Symlink was not removed."
+                    fi
+
+                    # Remove from pages list
+                    ### NEW FEATURE v3.0 ###
+
+                    # Reload ngnix
+                    sudo service nginx restart |& tee -a "${logPath}log_${currentData}.log" &> /dev/null
+                    flash "Reload nginx server"
+
+                    flash -i "Sucessfull, Now you haven't $remName vhost!"
+
+                else
+                    flash -d "You don't agree on delete, sory ..."
+                fi
+            fi
+
+        ;;
+        "list")
+            # LISTA VHOSTOW
+            ### NEW FEATURE v3.0 ###
+            exit 1;
+        ;;
+        *)
+            flash -d "Something went wrong! Please try again, remember about params..."
+    esac
 fi
